c364e0acc964bffd36e2014437be5c42e9ffd272
Christa Hartsock <christa.hartsock@gmail.com>
2015-05-09 15:21:28 -0700

    LET'S DO THIS

---
+++ config/common.py
@@ -0,0 +1,10 @@
+import os
+
+twitter_cred = [
+    os.environ.get('MASTER_BOT_CONSUMER_KEY'),
+    os.environ.get('MASTER_BOT_CONSUMER_SECRET'),
+    os.environ.get('YOUAREHERE_ACCESS_TOKEN'),
+    os.environ.get('YOUAREHERE_ACCESS_TOKEN_SECRET')
+]
+
+# BOT_ID = 2951110814
