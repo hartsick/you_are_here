652081e39c00d58b641159ed8be253b46c798ce4
Christa Hartsock <christa.hartsock@gmail.com>
2015-05-10 21:44:04 -0700

    remove char

--- twitter/gen_tweet.py
+++ twitter/gen_tweet.py
@@ -7,7 +7,7 @@ def gen_response(status):
         if status.location():
             text = "Never fear, you are here:"
         else:
-            text = "¯\_(ツ)_/¯ . Maybe verify location is enabled for tweets and ask again https://twitter.com/youareherebot/status/597616083386781696? Or, uh, just look around?"
+            text = "Maybe verify location is enabled for tweets and ask again? https://twitter.com/youareherebot/status/597616083386781696 Or, uh, just lookaround?"
     else:
         text = "You're doing it wrong. Try checking out this tweet for directions: https://twitter.com/youareherebot/status/597616083386781696"
 
